39ce0f2922cd447085e45e07fc1ec209d5bca7cd
Abel <ghazinyan.abel@gmail.com>
2017-07-11 22:42:36 +0400

    PSR-1/2 and bug fixes

--- index.php
+++ index.php
@@ -5,21 +5,20 @@
      * Date: 7/9/17
      * Time: 9:38 PM
      */
-    $file = file_get_contents("text");
-    $words = preg_split('/\s+/',$file);
-    foreach ($words as &$word){
-        $word = strtolower($word);
-        $word = str_replace(".","",$word);
-        $word = str_replace(",","",$word);
-    }
-    foreach ($words as $word){
-        if(!in_array($word,$words)) {
-            $frequency[$word] = 1;
-        }else{
-            $frequency[$word]++;
-        }
-    }
-php?>
+$file = file_get_contents("text");
+$words = preg_split('/\s+/',$file);
+foreach ($words as &$word) {
+    $word = strtolower($word);
+    $word = str_replace(".","",$word);
+    $word = str_replace(",","",$word);
+}
+foreach ($words as $word) {
+    $frequency[$word] = 0;
+}
+foreach ($words as $word) {
+    $frequency[$word]++;
+}
+?>
 <html>
 <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
 <script type="text/javascript">
@@ -34,11 +33,11 @@ php?>
     data.addColumn('number', 'Quantity');
     <?php
       echo "data.addRows([";
-      foreach ($frequency as $key => $value){
-        echo "['{$key}',{$value}],";
+      foreach ($frequency as $key => $value) {
+          echo "['{$key}',{$value}],";
       }
       echo "]);";
-    php?>
+    ?>
     var options = {'title':'Word Frequencies in the Lorem Ipsum',
                    'width':"100%",
                    'height':"100%"};
